fe1a9369f8064449bb6c26fc4e9da10890b9e73b
katevision <k.skrynnyk@gmail.com>
2026-06-26 17:35:46 -0400

    Update lightbox.js

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,11 +1,9 @@
 window.addEventListener('load', function() {
-  document.querySelectorAll('.my-popup').forEach(function(el) {
+  document.querySelectorAll('.my-lightbox').forEach(function(el) {
     el.addEventListener('click', function(e) {
       e.preventDefault();
-      var img = document.getElementById('lightbox-img');
-      var box = document.getElementById('lightbox');
-      img.src = this.href;
-      box.style.display = 'flex';
+      document.getElementById('lightbox-img').src = this.href;
+      document.getElementById('lightbox').style.display = 'flex';
     });
   });
   document.getElementById('lightbox').addEventListener('click', function() {
